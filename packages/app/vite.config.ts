03f35633a7b5ca6a9f51ac7b7d05526845745d85
soulsam480 <soulsam480@hotmail.com>
2021-04-16 20:10:16 +0530

    chore: now a monorepo

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -4,7 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001,
+    port: 4001,
     open: true,
   },
   plugins: [reactRefresh()],
