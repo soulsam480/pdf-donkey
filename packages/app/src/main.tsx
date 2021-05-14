83d5dd8285d640377e194e34196bf7ec806e58e8
soulsam480 <soulsam480@hotmail.com>
2021-05-14 18:08:33 +0530

    feat: add PWA caching

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,11 +1,15 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 // import 'shoyo/dist/main.css';
 import { Global } from './styles/globalStyle';
 import AppNavbar from './components/AppNavbar';
 
+registerSW({
+  immediate: true,
+});
 ReactDOM.render(
   <React.StrictMode>
     <Global />
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,14 +1,48 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+  },
   server: {
     port: 4001,
     open: true,
   },
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [
+    reactRefresh(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      strategies: 'generateSW',
+      workbox: {
+        globIgnores: ['_redirects'],
+        skipWaiting: true,
+        clientsClaim: true,
+        sourcemap: false,
+      },
+      manifest: {
+        scope: '.',
+        name: 'PDF Donkey',
+        short_name: 'PDF Donkey',
+        description: 'Open source on demand pdf generation.',
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#e2f3f5',
+        theme_color: '#22d1ee',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       src: '/src',
