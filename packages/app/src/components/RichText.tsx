9030ab358526a21761fa2c6e45e5ae3fa6609f88
soulsam480 <soulsam480@hotmail.com>
2021-05-20 22:13:39 +0530

    feat(app): Moved to tinymce for wysiwyg editor

--- packages/app/src/components/RichText.tsx
+++ packages/app/src/components/RichText.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import JoditEditor from 'jodit-react';
+import { Editor, IAllProps } from '@tinymce/tinymce-react';
 interface Props {
   code: string;
   setRichCode: (code: string) => void;
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const RichText: React.FC<Props> = ({ code, setRichCode, minHeight }) => {
-  const editor = useRef(null);
+  const editorRef = useRef<null | any>(null);
 
   return (
     <div
@@ -16,10 +16,45 @@ const RichText: React.FC<Props> = ({ code, setRichCode, minHeight }) => {
         position: 'relative',
       }}
     >
-      <JoditEditor
-        ref={editor}
+      <Editor
+        onInit={(evt, editor) => (editorRef.current = editor)}
         value={code}
-        onBlur={(newContent) => setRichCode(newContent)}
+        tinymceScriptSrc="https://cdn.jsdelivr.net/gh/soulsam480/self_hosted/tinymce.min.js"
+        onEditorChange={(val, editor) => setRichCode(val)}
+        init={{
+          max_height: minHeight,
+          height: minHeight,
+          menu: {
+            file: {
+              title: 'File',
+              items: 'print ',
+            },
+            view: {
+              title: 'View',
+              items: 'spellchecker | fullscreen',
+            },
+            insert: {
+              title: 'Insert',
+              items:
+                'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime',
+            },
+          },
+          menubar: 'file edit insert view format table',
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
+          ],
+          table_toolbar:
+            'tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
+          toolbar:
+            'undo redo | formatselect | tableinsertdialog' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
       />
     </div>
   );
