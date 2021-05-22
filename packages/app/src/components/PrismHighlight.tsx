67fae7debcb0f90e2fb9e02b8cc09c36bdfccd93
soulsam480 <soulsam480@hotmail.com>
2021-05-22 14:05:50 +0530

    feat(app): added auto formatting in code mode while editing templates

--- packages/app/src/components/PrismHighlight.tsx
+++ packages/app/src/components/PrismHighlight.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { highlight as Highlight } from 'highlight.js';
 import 'highlight.js/styles/dracula.css';
 import { ReactCodeJar } from 'react-codejar';
+import { html } from 'js-beautify';
 interface Props {
   language: string;
   code: string;
@@ -9,17 +10,18 @@ interface Props {
   minHeight: number;
 }
 
-const PrismHighlight: React.FC<Props> = ({
-  language,
-  code,
-  onCode,
-  minHeight,
-}) => {
+const PrismHighlight: React.FC<Props> = ({ language, code, onCode, minHeight }) => {
+  const [localCode, setLocalCode] = useState(code);
   const highlight = (editor: HTMLElement): Record<string, any> => {
     let code = editor.textContent as string;
     editor.innerHTML = Highlight(code, { language: language }).value;
     return {};
   };
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    timeout = setTimeout(() => onCode(html(localCode)), 2000);
+    return () => clearTimeout(timeout);
+  }, [localCode]);
   return (
     <ReactCodeJar
       style={{
@@ -31,7 +33,7 @@ const PrismHighlight: React.FC<Props> = ({
         maxHeight: `${minHeight}px`,
       }}
       code={code}
-      onUpdate={(e) => onCode(e)}
+      onUpdate={(e) => setLocalCode(e)}
       highlight={highlight}
     />
   );
